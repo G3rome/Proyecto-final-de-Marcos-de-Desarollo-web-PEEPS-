ae3a9ea9b8d52256be154509f3c889a2cf5855c8
Fabrizio <andrewmp2122@gmail.com>
2025-11-20 09:36:03 -0500

    Cambios: Ins-Mods: + 3 clases .java (Transaccion.java - PremiumController.java - ) + Creacion y correccion de la funcionalidad del pop de transacciones NOTA: Error de integracion casi final, pero con parte de avance, no rompe nada, solo que no completa la transaccion de mandar el usuario (no captura) a la tabla de transacciones para que se almacene con su plan y eso, y aparte sus botones en "/" que digan "Premium" y "Desuscribirse" -> Si/no > Si (Ejecuta como un delete from ...) y se desaparece esos botones, No > Continua normal

--- Marcos_de_Desarrollo_Web_Proyecto/src/main/java/com/example/demo/model/Transaccion.java
+++ Marcos_de_Desarrollo_Web_Proyecto/src/main/java/com/example/demo/model/Transaccion.java
@@ -1,8 +1,72 @@
 package com.example.demo.model;
 
-// Programacion de seccionamiento de panel premium: 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
 // Usuario -Adquiere-> Plan premium
 // Usuario -Cancela-> Plan premium (En evaluacion)
+@Entity
 public class Transaccion {
 
+    // Creacio de atributos de la clase -> tabla
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    private String planPremium;
+    private LocalDateTime fechaTransaccion;
+
+    // Constructor:
+    public Transaccion() {
+
+    }
+
+    // Constructor parametrizado capturando los atributos y metodos de la
+    // clase-entidad: Usuario
+    public Transaccion(Usuario usuario, String planPremium, LocalDateTime fechaTransaccion) {
+        this.usuario = usuario;
+        this.planPremium = planPremium;
+        this.fechaTransaccion = fechaTransaccion;
+    }
+
+    // Metodos Getters & Setters:
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPlanPremium() {
+        return planPremium;
+    }
+
+    public void setPlanPremium(String planPremium) {
+        this.planPremium = planPremium;
+    }
+
+    public LocalDateTime getFechaTransaccion() {
+        return fechaTransaccion;
+    }
+
+    public void setFechaTransaccion(LocalDateTime fechaTransaccion) {
+        this.fechaTransaccion = fechaTransaccion;
+    }
 }
